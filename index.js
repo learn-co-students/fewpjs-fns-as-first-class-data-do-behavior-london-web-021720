b68302ea2c2c034911850e81d131280fe15ca213
Amaterasus <skyetay03@googlemail.com>
2020-03-30 12:41:25 +0100

    Done.

--- index.js
+++ index.js
@@ -8,4 +8,21 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+
+function greet(time)
+{
+  const something = time.split(":")
+  const number = parseInt(something[0], 10)
+
+  return number > 0 && number < 12 ? "Good Morning" : 
+  number > 11 && number < 18 ? "Good Afternoon" : 
+  number > 17 ? "Good Evening" : "What planet are you on?"
+}
+
 /* Write your implementation of displayMessage() */
+
+function displayMessage(content) 
+{
+  const greeting = document.querySelector("#greeting");
+  greeting.innerText = content
+}
